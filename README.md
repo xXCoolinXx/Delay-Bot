08a6ab93e4c5d21db89807a414ec30c84d7eee4a
Coolin <collin.t.francel@gmail.com>
2022-02-04 16:23:23 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#DelayBot
+
+GroupMe bot that checks your county's Facebook page and posts it to the GroupMe if it contains info about school delays/closings.
+
+<h2>
+Setup
+<h2>
+Run `git clone https://github.com/xXCoolinXx/DelayBot`. In the cloned repository, you will need to create a `.env` file with the fields `FB_PAGE` and `BOT_ID` filled with your school/county's facebook page name and GroupMe bot ID. <br>
+
+To get a GroupMe bot, follow the instructions <a href=https://dev.groupme.com/tutorials/bots> here</a>. <br>
+
+In order to run, the bot will also need a `cookies.json` of your Facebook cookies. To obtain these, see the <a href=https://pypi.org/project/facebook-scraper>Facebook Scraper</a> website and scroll down to cookies.
---
+++ bot.py
@@ -0,0 +1,52 @@
+import datetime
+import requests
+from facebook_scraper import get_posts
+from requests.cookies import RequestsCookieJar
+from datetime import timedelta
+from datetime import datetime
+import http
+import os
+from time import sleep
+from dotenv import load_dotenv
+import asyncio
+
+load_dotenv() 
+TOKEN = os.getenv('BOT_ID')
+fb_page = os.getenv('FB_PAGE')
+keyword_list = ["closed", "open", "late", "delay"]
+url = f"https://api.groupme.com/v3/bots/post?bot_id={TOKEN}&text="
+
+def groupme_post(text):
+    requests.post(url + text)
+
+def check_fb_posts():
+    for post in get_posts(fb_page, pages=1, cookies="cookies.json"):
+        time_diff = datetime.now() - post['time']
+        print(post['time'])
+        if time_diff <= timedelta(minutes=5.10):
+            for kw in keyword_list:
+                if kw.lower() in post['text'].lower():
+                    print(post['text'][:50])
+                    groupme_post(\
+                        "🚨 New post from SCS 🚨\n" + \
+                        post['text'] + \
+                        "\nhttps://www.facebook.com/SumnerSchools/")
+                    break
+
+def check_loop():
+    while True:
+        print("Checking")
+        #It fails sometimes randomly so this is just a way to protect against that
+        try:
+            check_fb_posts()
+        except:
+            check_fb_posts() 
+        print("Sleeping")
+        sleep(5 * 60)
+
+def main():
+    asyncio.run(check_loop())
+
+
+if __name__ == "__main__":
+    main()
